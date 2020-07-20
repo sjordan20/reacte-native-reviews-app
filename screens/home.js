898da746e5d54702f822b47704c58e3995ca8310
sjordan20 <jordanstephen949@gmail.com>
2020-07-20 14:36:55 -0600

    first commit

---
+++ screens/home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titleText} >Home Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 100
+    },
+
+    titleText: {
+        fontFamily: 'nunito-bold'
+    }
+})
